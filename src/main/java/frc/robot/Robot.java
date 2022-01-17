29353907b59b0e1bdd6cbf9be75bc0e0c3cf0aea
Hirose-Yuto <hirose.y.aj@m.titech.ac.jp>
2022-01-17 14:24:18 +0900

    initial commit

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,35 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+
+public class Robot extends TimedRobot {
+  @Override
+  public void robotInit() {}
+
+  @Override
+  public void robotPeriodic() {}
+
+  @Override
+  public void autonomousInit() {}
+
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void teleopInit() {}
+
+  @Override
+  public void teleopPeriodic() {}
+
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  @Override
+  public void testInit() {}
+
+  @Override
+  public void testPeriodic() {}
+}
