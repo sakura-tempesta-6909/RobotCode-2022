c75c9189b6670f19fdb527dad66cdd3f83320d73
WilKen3 <wktanaka.13@gmail.com>
2022-02-23 14:15:52 +0900

    first commit

---
+++ src/main/java/frc/robot/component/Climb.java
@@ -0,0 +1,41 @@
+package frc.robot.component;
+
+public class Climb implements Component {
+
+  @Override
+  public void autonomousInit() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void teleopInit() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void disabledInit() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void testInit() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void readSensors() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void applyState() {
+    // TODO Auto-generated method stub
+    
+  }
+  
+}
