9fcc63bc25d263f16c89a4522d2a335e327127cd
Daiki-hayoshi <daiki.h@sakura-tempesta.org>
2022-03-06 19:15:24 +0900

    消えている部分があったので修正

--- src/main/java/frc/robot/mode/DriveMode.java
+++ src/main/java/frc/robot/mode/DriveMode.java
@@ -12,25 +12,33 @@ public class DriveMode extends Mode {
   public void changeMode() {
     if(driveController.getBackButton()){
       State.mode = Modes.k_conveyor;
-    }else if(driveController.getStartButton() && driveController.getBackButton()){
+    } else if(driveController.getStartButton() && driveController.getBackButton()){
       State.mode = Modes.k_climb;
     }
   
-  
-
-    if(driveController.getAButton()){
-      State.is_intakeExtendOpen = false;
-    }else{
-      State.is_intakeExtendOpen = true;
-    }
-
   }
 
   @Override
   public void changeState() {
-      // TODO Auto-generated method stub
+
+    State.driveSpeed = DriveSpeed.s_fastDrive;
+    State.driveXSpeed = driveController.getLeftY();
+    State.driveZRotation = driveController.getRightX();
+
+    if(driveController.getLeftBumper()){
+      State.conveyorState = ConveyorState.s_outtakeConveyor;
+    } else if(driveController.getRightBumper()){
+      State.conveyorState = ConveyorState.s_intakeConveyor;
+    }
       
+    if(driveController.getAButton()){
+      State.is_intakeExtendOpen = false;
+    } else {
+      State.is_intakeExtendOpen = true;
+    }
+
   }
-  
-  
+      
 }
+  
+  
