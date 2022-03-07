1344df4377cb57e02d24947c95d2d77f9a8b22cf
Daiki-hayoshi <daiki.h@sakura-tempesta.org>
2022-03-07 18:39:24 +0900

    driveRightのモーターの向きのへんこう

--- src/main/java/frc/robot/component/Drive.java
+++ src/main/java/frc/robot/component/Drive.java
@@ -31,8 +31,8 @@ public class Drive implements Component{
 
         driveRightFront.configAllSettings(Const.DriveRightConfig);
         driveLeftFront.configAllSettings(Const.DriveLeftConfig);
-        driveLeftFront.setInverted(true);
-        driveLeftBack.setInverted(true);
+        driveRightFront.setInverted(true);
+        driveRightBack.setInverted(true);
     }
     public void arcadeDrive(double xSpeed, double zRotation){
         differntialDrive.arcadeDrive(xSpeed, zRotation);
