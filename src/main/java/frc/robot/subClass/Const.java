7918e8bcf2c3735f6f191add71e66b07cdfd2e62
ddaiking <daidaiokasi@gmail.com>
2022-03-17 14:00:33 +0900

    add インデント

--- src/main/java/frc/robot/subClass/Const.java
+++ src/main/java/frc/robot/subClass/Const.java
@@ -11,89 +11,87 @@ public class Const {
 
     public static final class Ports{
         //PORTS
-    //ControllerPort(コントローラーのポート)
-    public static final int DriveController = 0;
-    public static final int OperateController = 1;
+        //ControllerPort(コントローラーのポート)
+        public static final int DriveController = 0;
+        public static final int OperateController = 1;
 
-    //Motor, drive
-    public static final int DriveRightFront = 0;
-    public static final int DriveRightBack= 1;
-    public static final int DriveLeftFront = 2;
-    public static final int DriveLeftBack = 3;
+        //Motor, drive
+        public static final int DriveRightFront = 0;
+        public static final int DriveRightBack= 1;
+        public static final int DriveLeftFront = 2;
+        public static final int DriveLeftBack = 3;
 
-    //Motor, Conveyor
-    public static final int IntakeBeltMotor = 4;
-    public static final int IntakeRoller = 5;
-    public static final int LaunchMotor = 6;
-    public static final int ConveyorExtend = 7;
-    public static final int BackPlate = 8;
-    public static final int LimitSwitch = 9;
+        //Motor, Conveyor
+        public static final int IntakeBeltMotor = 4;
+        public static final int IntakeRoller = 5;
+        public static final int LaunchMotor = 6;
+        public static final int ConveyorExtend = 7;
+        public static final int BackPlate = 8;
+        public static final int LimitSwitch = 9;
 
-    //sensor, conveyor
-    public static final int BallSensor =0; //これはあるか分からない
+        //sensor, conveyor
+        public static final int BallSensor =0; //これはあるか分からない
 
-    //pneumatics
-    public static final int Compressor = 0;
-    public static final int FirstSolenoid = 0;
-    public static final int SecondSolenoid = 1;
-    public static final int ClimbSolenoid = 2;
+        //pneumatics
+        public static final int Compressor = 0;
+        public static final int FirstSolenoid = 0;
+        public static final int SecondSolenoid = 1;
+        public static final int ClimbSolenoid = 2;
 
-    //sparkMax
-    public static final int ClimbArm = 1;
+        //sparkMax
+        public static final int ClimbArm = 1;
     }
     
     public static final class Speeds{
 
-    public static final double Neutral = 0;
+        public static final double Neutral = 0;
 
-    //DriveSpeed
-    public static final double FastDrive = 0.8;
-    public static final double MidDrive = 0.5;
-    public static final double SlowDrive = 0.25;
-    public static final double StopDrive = 0;
-    //ConveyorSpeed
-    //ボールの発射(Shoot)
-    public static final double BeltShoot = 0.3;
-    public static final double ShooterShoot = 1.0;
-    //ボールを出す(outtake)
-    public static final double BeltOuttake = 0.3;
-    public static final double RollerOuttake = 0.5;
-    public static final double ShooterOuttake = 0.5;
+        //DriveSpeed
+        public static final double FastDrive = 0.8;
+        public static final double MidDrive = 0.5;
+        public static final double SlowDrive = 0.25;
+        public static final double StopDrive = 0;
+        //ConveyorSpeed
+        //ボールの発射(Shoot)
+        public static final double BeltShoot = 0.3;
+        public static final double ShooterShoot = 1.0;
+        //ボールを出す(outtake)
+        public static final double BeltOuttake = 0.3;
+        public static final double RollerOuttake = 0.5;
+        public static final double ShooterOuttake = 0.5;
 
-    //ボールの回収(intake)
-    public static final double BeltIntake = 0.3;
-    public static final double RollerIntake = 0.5;
+        //ボールの回収(intake)
+        public static final double BeltIntake = 0.3;
+        public static final double RollerIntake = 0.5;
 
-     //intakeExtend
-     public static final double IntakeExtendOpen = 0.2;
+        //intakeExtend
+        public static final double IntakeExtendOpen = 0.2;
 
-     //Climb
-    public static final double FastClimbArmSpin = 1;
-    public static final double MidClimbArmSpin = 0.5;
+        //Climb
+        public static final double FastClimbArmSpin = 1;
+        public static final double MidClimbArmSpin = 0.5;
 
     }
    
 
-    public static final int ExtendPIDslot = 0;
-    public static final int UpPIDslot = 1;
-
-
     public static final class Xbox{
-    //Deadband
-    public static final double Deadband = 0.2;
-    //Trigger
-    public static final double TriggerValue = 0.7;
+        //Deadband
+        public static final double Deadband = 0.2;
+        //Trigger
+        public static final double TriggerValue = 0.7;
 
-    //Stick
-    public static final double RightXValue = 0.5;
+        //Stick
+        public static final double RightXValue = 0.5;
 
-    //POV
-    public static final double POV90Degrees = 90;
-    public static final double POV180Degrees = 180;
-    public static final double POV270Degrees = 270;
+        //POV
+        public static final double POV90Degrees = 90;
+        public static final double POV180Degrees = 180;
+        public static final double POV270Degrees = 270;
     }
     
-
+    public static final int ExtendPIDslot = 0;
+    public static final int UpPIDslot = 1;
+    
     public static final TalonSRXConfiguration DriveRightConfig = new TalonSRXConfiguration();
     public static final TalonSRXConfiguration DriveLeftConfig = new TalonSRXConfiguration();
     public static final TalonSRXConfiguration LaunchMotorConfig = new TalonSRXConfiguration();
