1c6fd1b321cd7e858e55b63ebd5243b2889a398d
Teraterayuki <tenko0316yuki1@gmail.com>
2022-03-19 05:42:03 +0900

    意味ないんかーーーい

--- src/main/java/frc/robot/component/Climb.java
+++ src/main/java/frc/robot/component/Climb.java
@@ -1,11 +1,7 @@
 package frc.robot.component;
 
-// import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
-// import com.revrobotics.SparkMaxAlternateEncoder;
-
-// import edu.wpi.first.hal.CTREPCMJNI;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
