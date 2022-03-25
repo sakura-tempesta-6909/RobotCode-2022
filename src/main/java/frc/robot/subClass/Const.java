49de16841fe1f1edff68e3676774cd8f3e7363ae
Daiki-hayoshi <daiki.h@sakura-tempesta.org>
2022-03-25 21:49:39 +0900

    Const.javaに//のあとのスペースが入れる

--- src/main/java/frc/robot/subClass/Const.java
+++ src/main/java/frc/robot/subClass/Const.java
@@ -7,36 +7,36 @@ import edu.wpi.first.math.util.Units;
 
 public class Const {
     public static final class Ports{
-        //PORTS                                          
-        //ControllerPort(コントローラーのポート)
+        // PORTS                                          
+        // ControllerPort(コントローラーのポート)
         public static final int DriveController = 0;
         public static final int OperateController = 1;
 
-        //Motor, drive
+        // Motor, drive
         public static final int DriveRightFront = 0;
         public static final int DriveRightBack= 1;
         public static final int DriveLeftFront = 2;
         public static final int DriveLeftBack = 3;
 
-        //Motor, Conveyor
+        // Motor, Conveyor
         public static final int IntakeBeltMotor = 4;
         public static final int IntakeRoller = 5;
         public static final int ShooterMotor = 6;
 
-        //sensor, conveyor
+        // sensor, conveyor
         public static final int BallSensor = 0; //これはあるか分からない
 
-        //pneumatics
+        // pneumatics
         public static final int Compressor = 0;
         public static final int FirstSolenoid = 0;
         public static final int SecondSolenoid = 1;
         public static final int ClimbSolenoid = 2;
         public static final int ConveyorExtend = 3;
 
-        //sparkMax
+        // sparkMax
         public static final int ClimbArm = 1;
 
-        //dio port
+        // dio port
         public static final int hallsensorPort = 1;
     }
 
@@ -47,39 +47,39 @@ public class Const {
     public static final class Speeds{
         public static final double Neutral = 0;
 
-        //DriveSpeed
-        //fastDriveの時のスピード
+        // DriveSpeed
+        // fastDriveの時のスピード
         public static final double FastDrive = 0.8;
-        //midDriveの時のスピード
+        // midDriveの時のスピード
         public static final double MidDrive = 0.5;
-        //slowDriveの時のスピード
+        // slowDriveの時のスピード
         public static final double SlowDrive = 0.25;
 
-        //ConveyorSpeed
-        //ボールの発射(Shoot)
-        //CARGOを発射するときのbeltのスピード
+        // ConveyorSpeed
+        // ボールの発射(Shoot)
+        // CARGOを発射するときのbeltのスピード
         public static final double BeltShoot = 0.3;
-        //CARGOを発射するときのshooterのスピード
+        // CARGOを発射するときのshooterのスピード
         public static final double ShooterShoot = 1.0;
 
-        //ボールを出す(outtake)
-        //outtakeするときのbeltのスピード
+        // ボールを出す(outtake)
+        // outtakeするときのbeltのスピード
         public static final double BeltOuttake = 0.3;
-        //outtakeするときのrollerのスピード
+        // outtakeするときのrollerのスピード
         public static final double RollerOuttake = 0.5;
-        //outtakeするときのshooterのスピード
+        // outtakeするときのshooterのスピード
         public static final double ShooterOuttake = 0.5;
 
-        //ボールの回収(intake)
-        //intakeするときのbeltのスピード
+        // ボールの回収(intake)
+        // intakeするときのbeltのスピード
         public static final double BeltIntake = 0.3;
         //intakeするときのbeltのスピード
         public static final double RollerIntake = 0.5;
 
-        //Climb
-        //fastClimbArmのスピード
+        // Climb
+        // fastClimbArmのスピード
         public static final double FastClimbArmSpin = 1;
-        //midClimbArmのスピード
+        // midClimbArmのスピード
         public static final double MidClimbArmSpin = 0.5;
     }
 
@@ -94,7 +94,7 @@ public class Const {
         // 1m進むとどのくらいPointが増えるか
         public static final double DrivePointsPerDriveLength = EncoderPointsPerRevolution / DriveLengthPerWheelRevolution;
 
-        //ClimbPoint
+        // ClimbPoint
         // 円の角度
         public static final double FullTurnAngle = 360;
         // ClimbArmのギア比
@@ -106,15 +106,15 @@ public class Const {
     }
 
     public static final class Other{
-        //シューターのモーターの最大速度
+        // シューターのモーターの最大速度
         public static final int shooterMotorMaxOutput = 100000;
 
-        //Deadband
+        // Deadband
         public static final double Deadband = 0.2;
-        //Triggerの押し込み具合
+        // Triggerの押し込み具合
         public static final double TriggerValue = 0.5;
 
-        //ClimbArmのモーターのAmpの制限値
+        // ClimbArmのモーターのAmpの制限値
         public static final int ClimbArmCurrentLimit = 60;
     }
 
