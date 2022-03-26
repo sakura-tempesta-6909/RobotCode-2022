0100c95f01a8ab0c6c65bd96b5993ac0a9992d2c
Hirose-Yuto <hirose.y.aj@m.titech.ac.jp>
2022-03-26 12:07:15 +0900

    delete test mode change state

--- src/main/java/frc/robot/mode/TestMode.java
+++ src/main/java/frc/robot/mode/TestMode.java
@@ -6,34 +6,14 @@ import frc.robot.State.ClimbArmState;
 public class TestMode extends Mode{
 
     @Override
-    public void changeMode() {
-        // TODO Auto-generated method stub
-        
-    }
+    public void changeMode() {}
 
     @Override
     public void changeState() {
-
-    if(driveController.getAButton()) {
-      State.climbArmState = ClimbArmState.s_setClimbArmAngle;
-      State.climbArmTargetAngle = 0;
-    } else if(driveController.getBButton()) {
-      State.climbArmState = ClimbArmState.s_setClimbArmAngle;
-      State.climbArmTargetAngle = 156;
-    } else if(driveController.getYButton()) {
-      State.climbArmState = ClimbArmState.s_setClimbArmAngle;
-      State.climbArmTargetAngle = 336;
-    } else if(driveController.getXButton()) {
-      State.climbArmState = ClimbArmState.s_setClimbArmAngle;
-      State.climbArmTargetAngle = 270;
-    }
-      
-
     if(driveController.getBackButton()){
       State.climbArmState = ClimbArmState.s_setClimbArmAngle;
       State.climbArmTargetAngle = 122.3;
     }
-        // TODO Auto-generated method stub
         
     if(driveController.getStartButton()){
         State.climbArmState = ClimbArmState.s_angleCalibration;
