d0dacebac2351a6f82f17396d1899813b541bbf2
WilKen3 <wktanaka.13@gmail.com>
2022-03-28 13:56:13 +0900

    aa

--- src/main/java/frc/robot/phase/Autonomous.java
+++ src/main/java/frc/robot/phase/Autonomous.java
@@ -16,31 +16,31 @@ public class Autonomous {
 		phaseTransition.registerPhase(
 			new PhaseTransition.Phase(
 				() -> {
-					State.is_compressorEnabled = true;
-					State.conveyorState = State.ConveyorState.s_intakeConveyor;
+					State.conveyorState = ConveyorState.s_shooterShoot;
 					return;
 				},
 				(double time) -> {
-					return time > 0.5;
+					return time > 1.0; //ここは調整
 				},
 				() -> {
-					System.out.println("on success");
-					State.conveyorState = State.ConveyorState.s_shooterShoot;
+					System.out.println("initial shot fired");
 				},
-				"intake"
+				"initial shot"
 			),
 			new PhaseTransition.Phase(
 				() -> {
-					State.is_compressorEnabled = false;
-					State.conveyorState = State.ConveyorState.s_intakeConveyor;
+					State.conveyorState = ConveyorState.s_intakeConveyor;
 					State.driveState = DriveState.s_pidDrive;
 					State.drivePidSetMeter = 2;
 					return;
 				},
 				(double time) -> {
-					return time > 2;
+					return (State.driveLeftFrontPositionMeter == 2) && (State.driveRightFrontPositionMeter == 2);
 				},
-				"wait for 2 sec"
+				() -> {
+					System.out.println("out of tarmac");
+				},
+				"out of tarmac"
 			),
 			new PhaseTransition.Phase(
 				() -> {
@@ -49,7 +49,7 @@ public class Autonomous {
 					return;
 				},
 				(double time) -> {
-					return time > 2;
+					return (true);
 				},
 				"shoot"
 			)
