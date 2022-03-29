310d6b7e1848195a49cf6e50888354fcb06b1f53
iwaikouki <koki4302000@gmail.com>
2022-03-29 20:06:32 +0900

    add DrivePidTolerance into Const

--- src/main/java/frc/robot/subClass/Const.java
+++ src/main/java/frc/robot/subClass/Const.java
@@ -168,6 +168,9 @@ public class Const {
 
         public static final double TestTurnDirection = 90;
 
+        //DrivePIDの目標値と現在の値の誤差の許容範囲(単位メートル)
+        public static final double DrivePidTolerance = 0.1;
+
     }
 
     public static final class MotorConfigs {
