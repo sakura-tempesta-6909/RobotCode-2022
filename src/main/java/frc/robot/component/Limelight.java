2150235f7020560403de058b64eedd52a7c29b5f
Daiki-hayoshi <daiki.h@sakura-tempesta.org>
2022-12-23 17:10:24 +0900

    距離を正確にする

--- src/main/java/frc/robot/component/Limelight.java
+++ src/main/java/frc/robot/component/Limelight.java
@@ -27,15 +27,15 @@ public class Limelight implements Component{
     }
     public void readSensors(){
 
-        double targetOffsetAngle_Vertical = entry.getDouble(0.0);  
+        double targetOffsetAngle_Vertical = 45 - entry.getDouble(0.0);  
         // how many degrees back is your limelight rotated from perfectly vertical?
-        double limelightMountAngleDegrees = 25.0;
+        double limelightMountAngleDegrees = 36.0;
         
         // distance from the center of the Limelight lens to the floor
-        double limelightLensHeightInches = 20.0;
+        double limelightLensHeightInches = 80.0;
         
         // distance from the target to the floor
-        double goalHeightInches = 60.0;
+        double goalHeightInches = 105.0;
         
         double angleToGoalDegrees = limelightMountAngleDegrees + targetOffsetAngle_Vertical;
         double angleToGoalRadians = angleToGoalDegrees * (3.14159 / 180.0);
